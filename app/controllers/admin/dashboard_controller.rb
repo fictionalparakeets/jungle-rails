0adc7af68fa404066b80b2388e2d73471e765973
fictionalparakeets <banksmatthew32@gmail.com>
2021-08-18 20:37:47 +0000

    add http basic auth for admin users

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with :name => ENV['HTTP_AUTH_USERNAME'], :password => ENV['HTTP_AUTH_PASSWORD']
+
+
   def show
   end
 end
