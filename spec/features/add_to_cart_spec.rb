0e791b8799d33304898e4a99d7f5c60c987790d1
fictionalparakeets <banksmatthew32@gmail.com>
2021-08-24 18:51:13 +0000

    add feature spec for adding items to cart

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They click on a product and are taken to the product detail" do
+    # ACT
+    visit root_path
+
+    # visit it and then click one of the product partials in order to navigate directly to a product detail page.
+    first('.product').click_on 'Add'
+
+    sleep(10)
+    # DEBUG / VERIFY
+    save_screenshot
+
+    expect(page).to have_content('My Cart (1)')
+  end
+
+end
